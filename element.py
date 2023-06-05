18d822ad93aefd810d59844788a4218fa560e20e
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2023-06-05 03:59:23 +0200

    Update element.py

--- element.py
+++ element.py
@@ -1,4 +1,7 @@
 import uuid
+import json
+
+
 
 class Element:
     """The base class of all page elements"""
@@ -26,6 +29,14 @@ class Element:
     def as_dict(self):
         return {x: self.__dict__[x] for x in self.__dict__ if self.__dict__[x] is not None}
         
+
+
+class ElementJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Element):
+            return obj.as_dict()
+
+
 #all_element_objects = Element.all_elements
 #for element in all_element_objects:
     # Hier kannst du auf jedes Element zugreifen und entsprechende Aktionen durchführen
