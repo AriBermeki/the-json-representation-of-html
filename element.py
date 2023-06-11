f63117baa0f933f78f663edd213d74c06624073f
AriBermeki <ari.bermeki.de@egmail.com>
2023-06-11 02:38:34 +0200

    first system

--- element.py
+++ element.py
@@ -2,42 +2,91 @@ import uuid
 import json
 
 
-
 class Element:
-    """The base class of all page elements"""
+    def __init__(self, tag, properties=None, attributes=None, eventListeners=None, content=None):
+        self._tag = tag
+        self._properties = properties or {}
+        self._attributes = attributes or {}
+        self._eventListeners = eventListeners or {}
+        self._content =  content
 
-    # Klassenvariable zur Speicherung der Element-Objekte
-    all_elements = []
+    @property
+    def tag(self):
+        return self._tag
 
-    def __init__(self, type_, content=None, id=None, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        self.uuid = str(uuid.uuid1())
-        self.component = type_
-        self.id = id
-        self.content = content
+    @property
+    def properties(self):
+        return self._properties
 
-        # Hinzufügen des aktuellen Objekts zur Liste aller Elemente
-        Element.all_elements.append(self.as_dict())
+    @property
+    def attributes(self):
+        return self._attributes
 
-    def __enter__(self):
-        return self
+    @property
+    def eventListeners(self):
+        return self._eventListeners
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        pass
+    @property
+    def content(self):
+        return self._content
 
-    def as_dict(self):
-        return {x: self.__dict__[x] for x in self.__dict__ if self.__dict__[x] is not None}
-        
+    def to_json(self):
+        data = {
+            "tag": self.tag,
+            "properties": self.properties,
+            "attributes": self.attributes,
+            "eventListeners": self.eventListeners,
+            "content":None
+        }
+
+        if isinstance(self.content, Element):
+            data["content"] = self.content.to_json()
+        else:
+            data["content"] = self.content
+
+        return data
 
 
-class ElementJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Element):
-            return obj.as_dict()
 
 
-#all_element_objects = Element.all_elements
-#for element in all_element_objects:
-    # Hier kannst du auf jedes Element zugreifen und entsprechende Aktionen durchführen
-    #print(element)
+elemnt = Element(
+    tag="div",
+    properties={
+        "id": "content",
+        "class": "content-class",
+        "style": "background-color: blue; color: white;"
+    },
+    attributes={
+        "data-action": "submit"
+    },
+    eventListeners={
+        "eventType": "copy",
+        "eventArgs": "console.log(event)",
+        "elementId": "myButton",
+        "callbackUuid": "iafsifoeipjeofw",
+        "sendToServer": True
+    },
+    content=Element(
+        tag="button",
+        properties={
+            "id": "myButton",
+            "class": "btn",
+            "style": "background-color: blue; color: white;"
+        },
+        attributes={
+            "data-action": "submit"
+        },
+        eventListeners={
+            "eventType": "click",
+            "eventArgs": "console.log(event)",
+            "elementId": "myButton",
+            "callbackUuid": "iafsifoeipjeofw",
+            "sendToServer": True
+        },
+        content="Click me!"
+    )
+)
+
+json_data = elemnt.to_json()
+json_str = json.dumps(json_data, indent=4)
+print(json_str)
